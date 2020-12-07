450404a3ebe894bc4aa95ed6f891153d6a597b88
Jordi Ustrell-Garrigos <jordiustrell@hotmail.com>
2020-12-07 10:50:40 +0100

    refactor removed old classes

--- src/main/java/com/merkle/kata/apirest/App.java
+++ src/main/java/com/merkle/kata/apirest/App.java
@@ -1,15 +1,7 @@
 package com.merkle.kata.apirest;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.merkle.kata.apirest.entity.Coordinate;
-import com.merkle.kata.apirest.entity.Obstacles;
-import com.merkle.kata.apirest.entity.PlanetGrid;
-import com.merkle.kata.apirest.entity.Rover;
-import com.merkle.kata.apirest.enums.Direction;
 
 @SpringBootApplication
 public class App {
@@ -17,7 +9,6 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	
-		
 	}
 
 }
